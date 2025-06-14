c4e058450239c8f3c4a9275b4759329040cf0cf6
Jeeva Raju <86867551+Jeeva-EntryLevDev@users.noreply.github.com>
2025-06-14 19:51:55 +0530

    Delete Problems_SPringBeans directory

--- Problems_SPringBeans/src/main/java/com/javabasedconfiguration_Bean/Engine.java
+++
@@ -1,7 +0,0 @@
-package com.javabasedconfiguration_Bean;
-
-public class Engine {
-	public void start() {
-		System.out.println("Engine started");
-	}
-}
--- Problems_SPringBeans/src/main/java/com/problem2/Mobile.java
+++
@@ -1,5 +0,0 @@
-package com.problem2;
-
-public interface Mobile {
-	public void displayMessage();
-}
